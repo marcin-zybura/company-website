e6aec5195add13b0b8603643f5138be1199f4946
marcin <mar.zybura@gmail.com>
2018-11-27 12:12:38 +0100

    mobile navigation

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-function myFunction(x) {
+function hamburgerToggle(x) {
   x.classList.toggle("hamburger-toggle");
+  let navigationListMobile = document.getElementById("navigation-list--mobile");
+  navigationListMobile.classList.toggle("navigation-list--mobile-show");
 }
